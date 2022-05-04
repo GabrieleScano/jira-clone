6708f6b8a0afcdfbcf754b575c4316d267c935c0
gabri <gabrielescano.bcn@gmail.com>
2022-05-04 15:15:49 +0200

    Set theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
