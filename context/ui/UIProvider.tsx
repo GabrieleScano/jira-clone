8470f2a1fa11b77b23966d4ead590f84f2b32f3d
gabri <gabrielescano.bcn@gmail.com>
2022-05-31 19:21:06 +0200

    Add UIProvider

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,21 @@
+import { FC, useReducer } from 'react'
+import { UIContext, uiReducer} from './'
+
+export interface UIState {
+    sideMenuOpen: boolean
+}
+
+const UI_INITIAL_STATE: UIState = {
+    sideMenuOpen: false
+}
+
+export const UIProvider:FC = ({children}) => {
+    const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE)
+  return (
+    <UIContext.Provider value={{
+        sideMenuOpen: false
+    }}>
+        {children}
+    </UIContext.Provider>
+  )
+}
